07d79e2dd8a672a632751f95504a69eaf6d98600
Kaique Sousa <63821804+ShivKSF@users.noreply.github.com>
2022-10-17 21:08:55 -0300

    Add files via upload

---
+++ 03 - Scripts/produto.sql
@@ -0,0 +1,15 @@
+create table produto (
+	id INT,
+	nome VARCHAR(19),
+	valor VARCHAR(50)
+);
+insert into produto (id, nome, valor) values (1, 'Atum com cebola', '515,39');
+insert into produto (id, nome, valor) values (2, 'Napolitana', '836,23');
+insert into produto (id, nome, valor) values (3, 'Muçarela', '437,01');
+insert into produto (id, nome, valor) values (4, 'Atum com cebola', '820,82');
+insert into produto (id, nome, valor) values (5, 'Napolitana', '534,69');
+insert into produto (id, nome, valor) values (6, 'Baiana', '685,93');
+insert into produto (id, nome, valor) values (7, 'Baiana', '469,03');
+insert into produto (id, nome, valor) values (8, 'Quatro queijos', '016,88');
+insert into produto (id, nome, valor) values (9, 'Frango com catupiry', '956,18');
+insert into produto (id, nome, valor) values (10, 'Muçarela', '938,25');
